c53c4e5fcdda9bbd71e00447262f42525c8001dc
lttjames <33799375+lttjames@users.noreply.github.com>
2020-01-02 11:15:59 +0800

    Add files via upload

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -7,63 +7,76 @@ using namespace std;
 
 int  Read_data(double pos[])
 {
-	int i;
-	ifstream in("C:\\Users\\Administrator\\Documents\\Visual Studio 2010\\Projects\\test\\Input.txt");
+	int i,node;
+	ifstream in("C:\\Users\\Administrator\\Documents\\Visual Studio 2010\\Projects\\ncepu-master\\test\\Input.txt");
 	for(i=0;i<100;i++)
 	{
 		in>>pos[i];
 	}
-	for(i=0;i<100;i++)
-	{
-		if(pos[i]==100)
-			break;
-	}
+	node=pos[0];
 	in.close();
-	return(i);
+	return(node);
+}
+
+void Form_Y(double ad[],int node)
+{
+	double G[10][10],B[10][10];
+	int i,j;
+	for(i=0;i<node;i++)
+	{
+		for(j=0;j<node;j++)
+		{
+			G[i][j]=0;
+			B[i][j]=0;
+		}
+	}
+	//初始化节点导纳矩阵
+	G[0][1]=-ad[25]/(ad[25]*ad[25]+ad[26]*ad[26]);
+	G[0][2]=-ad[30]/(ad[30]*ad[30]+ad[31]*ad[31]);
+	G[1][0]=G[0][1];
+	G[2][0]=G[0][2];
+	G[1][1]=-G[0][1];
+	G[2][2]=-G[0][2];
+	G[0][0]=-G[0][1]-G[0][2];
+	B[0][1]=ad[26]/(ad[25]*ad[25]+ad[26]*ad[26]);
+	B[0][2]=ad[31]/(ad[30]*ad[30]+ad[31]*ad[31]);
+	B[1][0]=B[0][1];
+	B[2][0]=B[0][2];
+	B[1][1]=-B[0][1];
+	B[2][2]=-B[0][2];
+	B[0][0]=-B[0][1]-B[0][2];
+	//形成节点导纳矩阵
+	cout<<"电导矩阵"<<endl;
+	for(i=0;i<node;i++)
+	{
+		for(j=0;j<node;j++)
+		{
+			cout<<G[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<"电纳矩阵"<<endl;
+	for(i=0;i<node;i++)
+	{
+		for(j=0;j<node;j++)
+		{
+			cout<<B[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 }
 
 void main()
 {
 	double ad[100];
-	int n,i,j,node;
+	int i,j,node;
 	for(i=0;i<100;i++)
 	{
-		ad[i]=100;
+		ad[i]=0;
 	}
-	n=Read_data(ad);
-	double G[10],B[10];
-	for(i=0;i<n;i++)
-	{
-		G[0]=ad[25];
-		G[1]=ad[30];
-		B[0]=ad[26];
-		B[1]=ad[31];
-		node=ad[0];
-	}
-	double gb[10][10];
-	for(i=0;i<node;i++)
-	{
-		for(j=0;j<node;j++)
-		{
-			gb[i][j]=0;
-		}
-	}
-	for(i=0;i<node;i++)
-	{
-		for(j=0;j<node;j++)
-		{
-			gb[0][0]=G[0]+G[1];
-			gb[1][1]=G[0];
-			gb[2][2]=G[1];
-		}
-	}
-	for(i=0;i<node;i++)
-	{
-		for(j=0;j<node;j++)
-		{
-			cout<<gb[i][j]<<" ";
-		}
-		cout<<endl;
-	}
+	//初始化输入数组
+	node=Read_data(ad);
+	//线路节点数
+	Form_Y(ad,node);
 	system("pause");
 }
